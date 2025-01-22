fb8ef43f2e6a8c36e5ddb88709d8d6de4740f118
Shiv Raj Badu <shivrajbadu@gmail.com>
2025-01-22 20:08:35 +0545

    Categories CRUD added

---
+++ categories/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import Category
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ['name']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'})
+        }
--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse
+from .models import Category
+from .forms import CategoryForm
 
-# Create your views here.
+def index(request):
+    categories = Category.objects.all()
+    return render(request, 'categories/index.html', {'categories': categories})
+
+def detail(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    return render(request, 'categories/detail.html', {'category': category})
+
+def create(request):
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('categories:index')
+    else:
+        form = CategoryForm()
+    return render(request, 'categories/form.html', {'form': form})
+
+def update(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    if request.method == 'POST':
+        form = CategoryForm(request.POST, instance=category)
+        if form.is_valid():
+            form.save()
+            return redirect('categories:index')
+    else:
+        form = CategoryForm(instance=category)
+    return render(request, 'categories/form.html', {'form': form})
+
+def delete(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    if request.method == 'POST':
+        category.delete()
+    return redirect('categories:index')
