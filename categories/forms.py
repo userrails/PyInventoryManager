0109a73522e807ee0fc494fcdc51a95fa8f6834a
Shiv Raj Badu <shivrajbadu@gmail.com>
2025-01-22 20:40:41 +0545

    background gray added on category textbox

--- categories/forms.py
+++ categories/forms.py
@@ -7,6 +7,6 @@ class CategoryForm(forms.ModelForm):
         fields = ['name']
         widgets = {
             'name': forms.TextInput(attrs={
-                'class': 'w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500'
+                'class': 'w-full px-3 py-2 bg-gray-100 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500'
             })
         }
