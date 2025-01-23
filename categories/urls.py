f30559167d262d5080abc43e2b782f8ad9383ac8
Shiv Raj Badu <shivrajbadu@gmail.com>
2025-01-23 14:07:13 +0545

    display scrapped products data to products list

--- categories/urls.py
+++ categories/urls.py
@@ -5,8 +5,9 @@ app_name = 'categories'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('create/', views.create, name='create'),
     path('<int:pk>/', views.detail, name='detail'),
+    path('create/', views.create, name='create'),
     path('<int:pk>/update/', views.update, name='update'),
     path('<int:pk>/delete/', views.delete, name='delete'),
+    path('products/', views.products, name='products'),
 ]
--- categories/views.py
+++ categories/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
-from .models import Category
+from .models import Category, Product
 from .forms import CategoryForm
 
 def index(request):
